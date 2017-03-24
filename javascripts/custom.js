0127602bd75b2debf1076a7cb7c9dce97f70c4cd
amoges1 <amoges1@gmail.com>
2017-03-23 23:14:14 -0400

    Refresh Site Design

--- javascripts/custom.js
+++ javascripts/custom.js
@@ -1,3 +1,20 @@
+
+//nav transition state
+$(window).scroll(function() {
+  	if($(document).scrollTop() > 10) {
+    	$('#nav').addClass('shrink');
+      $('.navbar-brand').addClass('larger');
+      $("#nav").addClass("navbar-inverse");
+    }
+    else {
+    $('#nav').removeClass('shrink');
+    $('.navbar-brand').removeClass('larger');
+    $("#nav").removeClass("navbar-inverse");
+
+    }
+  });
+
+//Scroll smooth animation
 $('a[href*="#"]:not([href="#"])').click(function() {
     if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
         var target = $(this.hash);
@@ -10,7 +27,26 @@ $('a[href*="#"]:not([href="#"])').click(function() {
         }
     }
 });
-$(".nav a").on("click", function() {
-    $(".nav").find(".active").removeClass("active");
-    $(this).parent().addClass("active");
+//highlight active section
+$(".nav li").on("click", function() {
+     $(".nav li").removeClass("active");
+     $(this).addClass("active");
+   });
+
+//Experience timeline objects
+$(".accordion").accordion({
+  header: ".timeline-heading",
+  content: ".timeline-body",
+  active:0,
+  collapsible: true,
+  icons: false,
+  heightStyle: 'content'
+});
+//Close navbar after selecting section (small display)
+$(function() {
+    $('.nav a').on('click', function(){
+        if($('.navbar-toggle').css('display') !='none'){
+            $(".navbar-toggle").trigger( "click" );
+        }
+    });
 });
